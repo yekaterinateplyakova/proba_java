37d03ee845a7240ffeff8c6de4ecc19d0963d89b
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-22 06:30:31 -0700

    Создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by kompu on 5/22/2016.
+ */
+public class MyFirstProgram {
+    public static void main(String[] arg){
+        System.out.println("Hello world");
+    }
+}
