92c08cc03219e5c18b104f6fafe5833748f9bfa8
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-22 12:58:58 -0700

    Добавлен reqtangle and squre with "this"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -11,4 +11,9 @@ public class Rectangle {
     this.b = b;
 
   }
+
+
+  public double area(){
+    return this.a * this.b;
+  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -8,4 +8,9 @@ public class Square {
   public Square(double l){
     this.l = l;
   }
+
+  public double area(){
+    return this.l * this.l;
+  }
+
 }
