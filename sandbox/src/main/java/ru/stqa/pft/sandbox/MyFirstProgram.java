cb42cadd891ba8023b6fe1d6466d1c58aaa5f352
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-25 18:17:45 -0700

    Calculator (3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,11 +5,6 @@ package ru.stqa.pft.sandbox;
  */
 public class MyFirstProgram {
 
-    System.out.println("Truck = " + price1.getTruckCount() );
-    System.out.println("Car = " + price1.getCarCount());
-    System.out.println("Bus = " + price1.getBusCount());
-    System.out.println("TotalCount  = " + price1.getTotalCount());
-    System.out.println("TotalSum = " + price1.getTotalSum());
 public static void main(String[] arg){
   Calculator calc = new Calculator();
   calc.type(2);
