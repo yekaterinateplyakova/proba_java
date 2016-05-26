52aa68aa499717df0fc477040c3439e3ddfbc2c7
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-25 18:14:57 -0700

    Calculator (2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,31 +4,6 @@ package ru.stqa.pft.sandbox;
  * Created by kompu on 5/22/2016.
  */
 public class MyFirstProgram {
-//  public static void main(String[] arg) {
-//
-//    Point p1 = new Point(5, 2);
-//    Point p2 = new Point(4, 1);
-//    System.out.println("Расстояние между двумя точками вызванно функцией = " + distance(p1, p2));
-//    System.out.println("Расстояние мужду двумя точками вызванно методом =" + p1.distance(p2));
-//  }
-//
-//  public static double distance(Point p1, Point p2) {
-//    double distance;
-//    distance = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-//    return distance;
-//
-//  }
-
-
-  public static void main(String[] arg){
-    Transpander price1 = new Transpander(20,30,10);
-    price1.truck();
-    price1.truck();
-    price1.bus();
-    price1.bus();
-    price1.bus();
-    price1.car();
-    price1.car();
 
     System.out.println("Truck = " + price1.getTruckCount() );
     System.out.println("Car = " + price1.getCarCount());
@@ -42,12 +17,6 @@ public static void main(String[] arg){
   calc.mult(2);
   calc.addToMemory();
   System.out.print("Memory: " + calc.memory + ", display: " + calc.display);
-
-
-
-
-
-
 
 
   }
