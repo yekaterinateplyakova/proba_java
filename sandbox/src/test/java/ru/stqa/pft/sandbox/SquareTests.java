9063222ea72f225f31a1d93657903d984477b322
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-27 06:04:15 -0700

    Создан Тест Square

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by kompu on 5/27/2016.
+ */
+public class SquareTests {
+
+  @Test
+  public void testArea(){
+   Square s = new Square(5);
+    Assert.assertEquals(s.area(),25.0);
+  }
+
+
+
+
+
+}
