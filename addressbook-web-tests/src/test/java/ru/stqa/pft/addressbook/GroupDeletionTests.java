b5c1478ed325fb9c52cc444b743ab57934a08779
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-05-29 05:45:31 -0700

    Создан TestBase and GoupDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testsGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroups();
+    }
+
+
+}
