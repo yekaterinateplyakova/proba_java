49dfbc43d039676e5230fe5b10229f4ea1fa45cb
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-06-12 14:26:15 -0400

    sort lists before compare

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CgroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CgroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.security.acl.Group;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class CgroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test1", "test2", "test3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test3", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().subminGroupModification();
     app.getNavigationHelper().gotoGroupPage();
@@ -30,7 +31,12 @@ public class CgroupModificationTests extends TestBase{
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
 
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -25,6 +25,7 @@ public class GroupCreationTests extends TestBase {
 //                max = g.getId();
 //            }
 //        }
+
         group.setId(after.stream().max(((o1, o2) -> Integer.compare(o1.getId(), o2.getId()))).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
