531ab0e1000f1cb46dff05b569c518cb5a438959
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-06-12 10:08:54 -0400

    getGroupCount method. before and after

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,9 +8,12 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testsGroupCreation() {
-        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
 
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
+        app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -13,7 +13,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
