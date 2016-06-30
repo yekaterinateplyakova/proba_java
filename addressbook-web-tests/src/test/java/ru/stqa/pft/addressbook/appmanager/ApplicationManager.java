d0b6145405adc90b411f5e1dc9b0de21423b5f8c
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-06-29 21:39:43 -0400

    описание тестового стенда вынесены в конф. файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,12 +7,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by kompu on 5/29/2016.
  */
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
@@ -22,10 +28,12 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
+
   }
-
-
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
     }
@@ -36,12 +44,12 @@ public class ApplicationManager {
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-    wd.get("http://localhost:8080/addressbook/group.php");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(this);
     navigationHelper = new NavigationHelper(this);
     contactHelper = new ContactHelper(this);
     sessionHelper = new SessionHelper(this);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -15,7 +15,8 @@ import ru.stqa.pft.addressbook.model.ContactData;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty(("browser"), BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
