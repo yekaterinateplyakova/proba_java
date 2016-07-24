b44f61dad6b31b9de3b03e18629782649a0c7516
yekaterinateplyakova <yekaterinateplyakova@gmail.com>
2016-07-24 10:43:00 -0400

    Manties-test is added

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+
+/**
+ * Created by kompu on 5/29/2016.
+ */
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty(("browser"), BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
+
+ }
